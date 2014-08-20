988bb222a2cd937123987edd5e51fa684a323af5
arnabpaul0333 <arnab.killer.paul9@gmail.com>
2014-08-20 19:46:01 +0530

    completed chapter6 validations and callbacks and chapter 7 in process

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
 
 
   def create 
-    @user = User.create(user_params)
+    @user = User.new(user_params)
     if @user.save
       redirect_to user_path(@user)
     else
@@ -26,10 +26,12 @@ class UsersController < ApplicationController
   def edit
     @user = User.find(params[:id]) 
   end
+
+
   def update
-    user = User.find(params[:id])
-    if user.update_attributes(user_params)
-      redirect_to user_path(user)
+    @user = User.find(params[:id])
+ if @user.update_attribute(:name, params[:user][:name])
+      redirect_to user_path(@user)
     else
       render 'edit'
     end
@@ -44,7 +46,7 @@ class UsersController < ApplicationController
 
   private 
   def user_params
-    params.require(:user).permit(:name,:email)
+    params.require(:user).permit(:name,:email, :password, :password_confirmation)
   end
 end
 
