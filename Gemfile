14b4b57fda7bbbffdc80a79a86efef921ae0a4a6
arnabpaul0333 <arnab.killer.paul9@gmail.com>
2014-08-21 19:46:28 +0530

    chapter9 in progress,completed editing and all users

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ gem 'rails', '4.0.8'
 gem 'bootstrap-sass', '2.3.2.0'
 gem 'sprockets', '2.11.0'
 gem 'bcrypt-ruby', '3.1.2'
+gem 'faker', '1.1.2'
 
 # Use mysql as the database for Active Record
 gem 'mysql2'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-
+before_action :signed_in_user, only: [:index, :edit, :update]
+before_action :correct_user,   only: [:edit, :update]
   def new
     @user = User.new
   end
@@ -31,13 +32,13 @@ class UsersController < ApplicationController
 
   def update
     @user = User.find(params[:id])
- if @user.update_attribute(:name, params[:user][:name])
+    if @user.update_attributes(user_params)
       redirect_to user_path(@user)
     else
       render 'edit'
     end
   end
-
+  
 
   def destroy
     User.find(params[:id]).destroy
@@ -48,6 +49,19 @@ class UsersController < ApplicationController
   private 
   def user_params
     params.require(:user).permit(:name,:email, :password, :password_confirmation)
+  end
+
+	
+  def signed_in_user
+    unless signed_in?
+      store_location
+      redirect_to new_session_path, notice: "Please sign in."
+    end
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless current_user?(@user)
   end
 end
 
